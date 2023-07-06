48928dad76228bfb17bd566fc97ef08aab8e902d
Scholar618 <107455043+Scholar618@users.noreply.github.com>
2023-07-06 16:36:07 +0800

    Create longestIncreaseSequence.cpp

---
+++ 动态规划/longestIncreaseSequence.cpp
@@ -0,0 +1,23 @@
+/*
+给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+*/
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) {
+            return 0;
+        }
+        vector<int> dp(n, 0);
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    } 
+};
